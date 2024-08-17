8b44ed6b93a39cf6d34d859dce8bc14011e90669
Alessandro Versace <aleversace98@gmail.com>
2024-08-17 23:34:50 +0200

    feat(targetPerc): add algo that checks if current price is lower than predefined targetPerc + removed cron (this will run on lambda)

--- go.mod
+++ go.mod
@@ -5,6 +5,5 @@ go 1.22
 require (
 	github.com/PuerkitoBio/goquery v1.9.2 // indirect
 	github.com/andybalholm/cascadia v1.3.2 // indirect
-	github.com/robfig/cron/v3 v3.0.1 // indirect
 	golang.org/x/net v0.28.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,34 +3,25 @@ package main
 import (
 	"fmt"
 	"github.com/PuerkitoBio/goquery"
-	"github.com/robfig/cron/v3"
 	"log"
 	"net/http"
-	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 const yahooFinanceURL = "https://finance.yahoo.com/quote/"
 
 const userAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+const bitSize = 64
+const targetPerc = -19.0
 
 func main() {
-	c := cron.New()
-
-	_, err := c.AddFunc("0,15,30,45 * * * *", func() {
-		priceJob()
-	})
-	if err != nil {
-		os.Exit(1)
-	}
-
-	c.Start()
-
-	select {}
+	priceJob()
 }
 
 func priceJob() {
-	tickers := [2]string{"BQE.V", "HAYPP.ST"}
+	tickers := [3]string{"BQE.V", "HAYPP.ST", "MOB.ST"}
 	client := &http.Client{}
 	for _, ticker := range tickers {
 		url := yahooFinanceURL + ticker + "/"
@@ -61,13 +52,40 @@ func priceJob() {
 func extractPrice(doc *goquery.Document, ticker string) {
 	// The stock price on the Yahoo Finance page is within a <fin-streamer> tag with a specific class
 	priceSelector := fmt.Sprintf("fin-streamer[data-field='regularMarketPrice'][data-symbol='%s']", ticker)
+	highLowPriceSelector := fmt.Sprintf("fin-streamer[data-field='fiftyTwoWeekRange'][data-symbol='%s']", ticker)
 	currencySelector := ".exchange > :last-child"
 
 	price := doc.Find(priceSelector).Text()
+	highLowPrice := doc.Find(highLowPriceSelector).Text()
+
+	parts := strings.Split(highLowPrice, " - ")
+	maxPrice := strings.TrimSpace(parts[1])
+
 	currency := doc.Find(currencySelector).Text()
-	if price != "" && currency != "" {
+	if price != "" && currency != "" && maxPrice != "" {
 		log.Printf("The current price of %s is: %s %s\n", ticker, price, currency)
+		log.Printf("52 weeks high of %s was: %s\n", ticker, maxPrice)
+
+		priceFloat, err := strconv.ParseFloat(price, bitSize)
+		if err != nil {
+			log.Fatal("Error converting price to float: ", err)
+			return
+		}
+		maxPriceFloat, err := strconv.ParseFloat(maxPrice, bitSize)
+		if err != nil {
+			log.Fatal("Error converting price to float: ", err)
+			return
+		}
+
+		checkTarget(priceFloat, maxPriceFloat)
 	} else {
 		log.Println("Could not find the stock price.")
+	}
+}
+
+func checkTarget(currentPrice float64, maxPrice float64) {
+	currentPerc := ((currentPrice - maxPrice) / maxPrice) * 100
+	if currentPerc <= targetPerc {
+		log.Printf("Target reached! %.2f", currentPerc)
 	}
 }
